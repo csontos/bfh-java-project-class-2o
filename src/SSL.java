0b3e10623763ff57e3dcc1ff6cee30552a1f5d85
csontos <robin.csontos@gmail.com>
2014-05-19 17:57:22 +0200

    Merge

--- src/SSL.java
+++ src/SSL.java
@@ -6,12 +6,11 @@ Klasse:
 Beschreibung: Das ist die Klasse für Arbeitgeber SSL= Schuldner steruerbarer Leistung
 
 */
-public class SSL {
+public class SSL implements Comparable{
 	public final static String DISCRIMINATOR = "SSL";
 	private static int ID; //wenn es diese nicht gibt, setzen
 	private static String Firmenname;
 	private static int Sitz;
-	
 	
 	public SSL(int iD, String firmenname, int sitz) {
 		
@@ -19,31 +18,51 @@ public class SSL {
 
 
 	public static SSL getSSL( List<String> values ) {  
-			
 		int NewID = 0;
+		String Name;
+		int bfs;
 			
 		if (values.size() == 2) {
-			// NewID = Collections.max(QuellenSteuer.getSsls().getID()); // Das
-			// müssen wir noch machen. Randomizaaa
+			if(QuellenSteuer.getSsls().size() == 0) {
+				NewID = 1;
+			} else {
+				Collections.sort(QuellenSteuer.getSsls());
+				int LargestId = QuellenSteuer.getSsls().get(QuellenSteuer.getSsls().size()-1).getID();
+				System.out.println(LargestId);
+				NewID = LargestId++;
+			}
+			Name = values.get(0);
+			bfs = Integer.parseInt(values.get(1));
 		} else if (values.size() == 3) {
 			NewID = Integer.parseInt(values.get(0));
+			Name = values.get(1);
+			bfs = Integer.parseInt(values.get(2));
 		} else {
 			throw new RuntimeException("Falsche Anzahl von Werten: "
 					+ values.size() + "\n" + format());
 		}
 		try {
 			int ID = NewID;
-			String Firmenname = values.get(1);
-			int Sitz = Integer.parseInt(values.get(2));
+			String Firmenname = Name;
+			int Sitz = bfs;
 			return new SSL(ID, Firmenname, Sitz);
 		} catch (RuntimeException r) {
 			throw new RuntimeException("Error: " + r.getMessage() + "\n"
 					+ format());
 		}
 	}
+	
+	public static int getID() {
+		return ID;
+	}
 
-	   private static String format() {
+	private static String format() {
 		      return "Erwartetes Format:\n"+ DISCRIMINATOR + ":" + " ID; Name; Sitz";
 		   }
-	   
+
+
+	public int compareTo(Object o) {
+		SSL that = (SSL)o;
+		return this.getID() - that.getID();
+	}	   
 }
