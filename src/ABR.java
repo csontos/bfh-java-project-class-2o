b4d582b84c4febe2fab80666c6f831141b65fb9e
csontos <robin.csontos@gmail.com>
2014-05-19 17:54:26 +0200

    Revert "Merge branch 'master' of https://github.com/csontos/bfh-java-project-class-2o"
    This reverts commit e0996089a8a7453a7043d79771c24ee433176098.

--- src/ABR.java
+++ src/ABR.java
@@ -7,7 +7,7 @@ Beschreibung:
 */
 
 
-public class ABR implements Comparable{
+public class ABR {
 
 	public final static String DISCRIMINATOR = "ABR";
 	private int ID;
@@ -28,48 +28,32 @@ public class ABR implements Comparable{
 
 	
 	   public static ABR getABR( List<String> values ) {
-		   	int NewID;
-		   	int tmpq;
-		   	int tmps;
-		   	int Jahr;
-		   	int Monat;
-		   	double Betrag;
-		   
-		   
-		   	if( values.size() == 5 ){
-		   		  //ID generieren (siehe SSL)
-		   	} else if( values.size() == 6 ) {
-		   		NewID = Integer.parseInt( values.get(0) ); // Hier noch die IDs ect.. von den Objekten implementieren
-		   		tmpq = Integer.parseInt(values.get(1));
-		    	//if (QUP.getID(values) // Prüfen ob QUP ID existiert?
-		    	//int Qup.ID = Integer.parseInt(values.get(1));
-		    	tmps = Integer.parseInt(values.get(2));
-		    	// int Ssl.ID = Integer.parseInt(values.get(2));
-		    	// Prüfen ob SSL ID existiert?
-		    	Jahr = Integer.parseInt(values.get(3));		      
-		    	Monat = Integer.parseInt(values.get(4));
-		    	Betrag = Double.parseDouble(values.get(5));  
-		      } else {
-		    	  throw new RuntimeException("Falsche Anzahl von Werten: " + values.size() + "\n" + format());
+		      if( values.size() != 6 ) {
+		         throw new RuntimeException("Falsche Anzahl von Werten: " + values.size() + "\n" + format());
 		      }
-		   	  try {
-		    	  
-		    	  //return new ABR( ID, Qup.ID, Ssl.ID, Jahr, Monat, Betrag );
-		    	  // return new ABR( ID, Qup.ID, Ssl.ID, Jahr, Monat, Betrag );
-		    	  return null;
+		      try {
+		      int ID = Integer.parseInt( values.get(0) ); // Hier noch die IDs ect.. von den Objekten implementieren
+		      int tmpq = Integer.parseInt(values.get(1));
+		      //if (QUP.getID(values) // Prüfen ob QUP ID existiert?
+		      //int Qup.ID = Integer.parseInt(values.get(1));
+		      int tmps = Integer.parseInt(values.get(2));
+		      // int Ssl.ID = Integer.parseInt(values.get(2));
+		      // Prüfen ob SSL ID existiert?
+		      int Jahr = Integer.parseInt(values.get(3));		      
+		      int Monat = Integer.parseInt(values.get(4));
+		      double Betrag = Double.parseDouble(values.get(5));
+		      //return new ABR( ID, Qup.ID, Ssl.ID, Jahr, Monat, Betrag );
+		     // return new ABR( ID, Qup.ID, Ssl.ID, Jahr, Monat, Betrag );
+		      return null;
 		      }
 		      catch(RuntimeException r) {
 		         throw new RuntimeException("Error: " + r.getMessage() + "\n" + format());
-		      }  
-	   }
+		      }
+		   }
+
 	   
 	   private static String format() {
 		      return "Erwartetes Format:\n"+ DISCRIMINATOR + ":" + " ID, QUP_ID; SSL_ID; Jahr; Monat; Betrag";
 		   }
-
-	public int compareTo(Object o) {
-		ABR that = (ABR)o;
-		return this.ID - that.ID;
-	}
 	
 }
