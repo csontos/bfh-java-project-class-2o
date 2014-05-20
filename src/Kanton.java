babdee1484951dde05612a449d0986997d9a11be
csontos <robin.csontos@gmail.com>
2014-05-20 12:49:22 +0200

    Class Kanton angepasst
    Alle Kantone aufgenommen mit kurzName, Name und Kanton Steuer

--- src/Kanton.java
+++ src/Kanton.java
@@ -7,10 +7,14 @@ Beschreibung:
 
 
 public enum Kanton {
-   AG, AI, AR, SZ, ZH;
+   AG, AI, AR, BE, BL, BS, FR, GE, GL, GR, JU, LU, NE, NW, OW, SG, SH, SO, SZ, TG, TI, UR, VD, VS, ZG, ZH;
    
-   private String[] longNames = { "Aargau", "Appenzell Innerhoden", "Appenzell Ausserhoden", "Schwyz", "Zürich"};
-   private Double[] kantSteuer = { 4.5, 4.5, 4.5, 4.0, 4.5 };
+   private String[] longNames = { "Aargau", "Appenzell Innerhoden", "Appenzell Ausserhoden", "Basel-Landschaft", 
+		   "Basel-Stadt", "Bern", "Fribourg", "Genf", "Glarus", "Graubünden", "Jura", "Luzern", "Neuenburg",
+		   "Nidwalden", "Obwalden", "Schaffhausen", "Schwyz", "Solothurn", "St. Gallen", "Thurgau", "Tessin",
+		   "Uri", "Wallis", "Waadt", "Zug", "Zürich"};
+   private Double[] kantSteuer = { 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 6.0, 5.0, 5.0, 6.0, 5.0, 5.0, 5.0, 5.0,
+		   5.0, 5.0, 5.0, 4.0, 5.0, 5.0, 5.0, 5.0, 6.0, 4.0, 5.0 };
    
    public String toString() {
       return longNames[this.ordinal()];
